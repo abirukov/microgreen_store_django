e18580a8a622a4e7ab6956ee6d7c4f40ae42bcd6
abirukov <abirukov2008@gmail.com>
2023-11-17 17:38:53 +0300

    fix style and type

--- microgreen_store/settings.py
+++ microgreen_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-v1yh+mm%lg1)r#&vioilsf^30$&tn*b)e-m)*(b_^y=!+-osgm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str | None] = []
 
 
 # Application definition
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login
-from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, UpdateView, ListView, DeleteView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from users.forms import RegisterForm
 
@@ -10,31 +11,10 @@ class RegisterView(CreateView):
     template_name = "users/test.html"
     success_url = reverse_lazy("<app_name>:<view_name>")
 
-    def form_valid(self, form):
+    def form_valid(self, form: RegisterForm) -> HttpResponseRedirect:
         user = form.save()
 
         if user:
             login(self.request, user)
 
         return super().form_valid(form)
-
-# class UserUpdateView(UpdateView):
-#     model = User
-#     form_class = UpdateTaskForm
-#     template_name = 'users/test.html'
-#
-#     def get_success_url(self) -> str:
-#         task_pk = self.kwargs.get('pk')
-#         return reverse('todos:tasks-update', kwargs={'pk': task_pk})
-
-
-# class UserListView(ListView):
-#     model = User
-#     template_name = 'users/test.html'
-#     context_object_name = 'users'
-#
-#
-# class UserDeleteView(DeleteView):
-#     model = User
-#     success_url = '/users/'
-#     template_name = 'users/test.html'
