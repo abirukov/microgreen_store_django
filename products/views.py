923d95db500c052ae249655ff15c590b46d1541a
abirukov <abirukov2008@gmail.com>
2023-11-23 18:57:08 +0300

    product list page

--- products/views.py
+++ products/views.py
@@ -6,6 +6,7 @@ from products.models import Product
 class ProductListView(ListView):
     model = Product
     template_name = "products/list.html"
+    context_object_name = 'products'
     paginate_by = 8
 
     def get_context_data(self, **kwargs):
