e6d8c6f8521d525ca9effe9f37c24cdda4e0ca76
abirukov <abirukov2008@gmail.com>
2023-11-30 15:40:20 +0300

    staticfiles fix

--- microgreen_store/settings.py
+++ microgreen_store/settings.py
@@ -27,7 +27,7 @@ config = get_config()
 SECRET_KEY = config.secret_key
 DEBUG = config.debug
 
-ALLOWED_HOSTS: list[str | None] = ["0.0.0.0"]
+ALLOWED_HOSTS: list[str | None] = ["127.0.0.1"]
 
 sentry_sdk.init(
     dsn=config.sentry_dsn,
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -136,8 +137,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
